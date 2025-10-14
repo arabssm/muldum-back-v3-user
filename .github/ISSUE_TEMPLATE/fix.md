d990e1c4e061b3eb96f38031f21059fe5f0fd83f
randirao <randirao4245@gmail.com>
2025-10-14 12:27:02 +0900

    chore: 템플릿 구조 반영 및 Gradle 프로젝트 설정

---
+++ .github/ISSUE_TEMPLATE/fix.md
@@ -0,0 +1,11 @@
+---
+name: fix
+about: 오류 수정
+title: ''
+labels: bug
+assignees: ''
+
+---
+
+# 오류 내용
+> 오류 내용을 작성해 주세요
---
+++ src/main/java/co/kr/muldum/MuldumApplication.java
@@ -0,0 +1,13 @@
+package co.kr.muldum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuldumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuldumApplication.class, args);
+    }
+
+}
