9401eedabd3b7e3aec5c88962d13dc4076d62bf1
randirao <randirao4245@gmail.com>
2025-10-16 10:40:25 +0900

    refactor: Lombok 적용 및 Optional 도입

--- src/main/java/co/kr/muldum/application/port/in/response/LoginResponse.java
+++ src/main/java/co/kr/muldum/application/port/in/response/LoginResponse.java
@@ -1,53 +1,27 @@
 package co.kr.muldum.application.port.in.response;
 
 import co.kr.muldum.domain.model.UserType;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+import java.util.Optional;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class LoginResponse {
     private final UserType userType;
     private final Long userId;
     private final String name;
-    private final Long teamId;
+    private final Optional<Long> teamId;
     private final String role;
     private final String accessToken;
 
-    private LoginResponse(UserType userType, Long userId, String name, Long teamId, String role, String accessToken) {
-        this.userType = userType;
-        this.userId = userId;
-        this.name = name;
-        this.teamId = teamId;
-        this.role = role;
-        this.accessToken = accessToken;
-    }
-
     public static LoginResponse ofStudent(Long userId, String name, Long teamId, String accessToken) {
-        return new LoginResponse(UserType.STUDENT, userId, name, teamId, "student", accessToken);
+        return new LoginResponse(UserType.STUDENT, userId, name, Optional.ofNullable(teamId), "student", accessToken);
     }
 
     public static LoginResponse ofTeacher(Long userId, String name, String accessToken) {
-        return new LoginResponse(UserType.TEACHER, userId, name, null, "teacher", accessToken);
-    }
-
-    public UserType getUserType() {
-        return userType;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Long getTeamId() {
-        return teamId;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public String getAccessToken() {
-        return accessToken;
+        return new LoginResponse(UserType.TEACHER, userId, name, Optional.empty(), "teacher", accessToken);
     }
 }
