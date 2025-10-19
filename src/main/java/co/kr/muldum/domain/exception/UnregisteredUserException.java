a0e8f0dc44e3e95edc74ae6c47160c6814af98fc
randirao <randirao4245@gmail.com>
2025-10-20 01:40:28 +0900

    refactor: BusinessException 상속으로 변경

--- src/main/java/co/kr/muldum/domain/exception/UnregisteredUserException.java
+++ src/main/java/co/kr/muldum/domain/exception/UnregisteredUserException.java
@@ -1,7 +1,13 @@
 package co.kr.muldum.domain.exception;
 
-public class UnregisteredUserException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+
+public class UnregisteredUserException extends BusinessException {
+
+    private static final String ERROR_CODE = "UNREGISTERED_USER";
+    private static final HttpStatus HTTP_STATUS = HttpStatus.CONFLICT;
+
     public UnregisteredUserException(String email) {
-        super("등록되지 않은 사용자입니다: " + email);
+        super("등록되지 않은 사용자입니다: " + email, HTTP_STATUS, ERROR_CODE);
     }
 }
