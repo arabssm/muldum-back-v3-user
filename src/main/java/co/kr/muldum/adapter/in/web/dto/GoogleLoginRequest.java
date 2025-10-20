88e4d62e21a08bbdd5d3035166d13fc9d9f734ce
randirao <randirao4245@gmail.com>
2025-10-20 14:06:45 +0900

    refactor: 불변 객체로 개선

--- src/main/java/co/kr/muldum/adapter/in/web/dto/GoogleLoginRequest.java
+++ src/main/java/co/kr/muldum/adapter/in/web/dto/GoogleLoginRequest.java
@@ -1,24 +1,20 @@
 package co.kr.muldum.adapter.in.web.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 
 public class GoogleLoginRequest {
 
     @NotBlank(message = "Authorization code is required")
-    private String authorizationCode;
+    private final String authorizationCode;
 
-    public GoogleLoginRequest() {
-    }
-
-    public GoogleLoginRequest(String authorizationCode) {
+    @JsonCreator
+    public GoogleLoginRequest(@JsonProperty("authorizationCode") String authorizationCode) {
         this.authorizationCode = authorizationCode;
     }
 
     public String getAuthorizationCode() {
         return authorizationCode;
-    }
-
-    public void setAuthorizationCode(String authorizationCode) {
-        this.authorizationCode = authorizationCode;
     }
 }
