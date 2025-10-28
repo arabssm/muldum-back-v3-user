d1e050203ed3f5dfebc6445d9467fd973ba559b4
randirao <randirao4245@gmail.com>
2025-10-28 17:37:20 +0900

    feat: 전공동아리 정보 없음 예외 추가

---
+++ src/main/java/co/kr/muldum/domain/exception/HistoryNotFoundException.java
@@ -0,0 +1,13 @@
+package co.kr.muldum.domain.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class HistoryNotFoundException extends BusinessException {
+
+    private static final String ERROR_CODE = "HISTORY_NOT_FOUND";
+    private static final HttpStatus HTTP_STATUS = HttpStatus.NOT_FOUND;
+
+    public HistoryNotFoundException(Integer generation) {
+        super(generation + "세대의 전공동아리 정보를 찾을 수 없습니다.", HTTP_STATUS, ERROR_CODE);
+    }
+}
