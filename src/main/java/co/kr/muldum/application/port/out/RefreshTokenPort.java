68be38260b1798763687e6576ca302657fa00193
randirao <randirao4245@gmail.com>
2025-11-03 19:20:31 +0900

    feat: RefreshToken 생성 Port 추가

---
+++ src/main/java/co/kr/muldum/application/port/out/RefreshTokenPort.java
@@ -0,0 +1,7 @@
+package co.kr.muldum.application.port.out;
+
+import co.kr.muldum.domain.model.RefreshToken;
+
+public interface RefreshTokenPort {
+    RefreshToken generateRefreshToken(Long userId);
+}
