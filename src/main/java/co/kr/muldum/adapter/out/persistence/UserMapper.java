efeb118c84479b6a645379de87a0510ae487735b
randirao <randirao4245@gmail.com>
2025-11-03 17:44:29 +0900

    refactor: UserMapper 단일화 및 도메인-엔티티 매핑 로직 통합

--- src/main/java/co/kr/muldum/adapter/out/persistence/UserMapper.java
+++ src/main/java/co/kr/muldum/adapter/out/persistence/UserMapper.java
@@ -1,7 +1,5 @@
 package co.kr.muldum.adapter.out.persistence;
 
-import co.kr.muldum.domain.model.Student;
-import co.kr.muldum.domain.model.Teacher;
 import co.kr.muldum.domain.model.User;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -9,41 +7,28 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserMapper {
 
-    public static User toDomain(UserJpaEntity entity) {
-        if (entity instanceof StudentJpaEntity studentEntity) {
-            return Student.of(
-                    studentEntity.getUserId(),
-                    studentEntity.getEmail(),
-                    studentEntity.getName(),
-                    studentEntity.getTeamId()
-            );
-        } else if (entity instanceof TeacherJpaEntity teacherEntity) {
-            return Teacher.of(
-                    teacherEntity.getUserId(),
-                    teacherEntity.getEmail(),
-                    teacherEntity.getName()
-            );
-        }
-
-        throw new IllegalArgumentException("Unknown entity type: " + entity.getClass().getName());
+    public static User toDomain(UserJpaEntity entity, Long teamId) {
+        return User.of(
+                entity.getId(),
+                entity.getName(),
+                entity.getEnrolledAt(),
+                entity.getEmail(),
+                entity.getClassNo(),
+                entity.getGrade(),
+                entity.getUserRole(),
+                teamId
+        );
     }
 
     public static UserJpaEntity toEntity(User user) {
-        if (user instanceof Student student) {
-            return new StudentJpaEntity(
-                    student.getUserId(),
-                    student.getEmail(),
-                    student.getName(),
-                    student.getTeamId()
-            );
-        } else if (user instanceof Teacher teacher) {
-            return new TeacherJpaEntity(
-                    teacher.getUserId(),
-                    teacher.getEmail(),
-                    teacher.getName()
-            );
-        }
-
-        throw new IllegalArgumentException("Unknown user type: " + user.getClass().getName());
+        return new UserJpaEntity(
+                user.getId(),
+                user.getName(),
+                user.getEnrolledAt(),
+                user.getEmail(),
+                user.getClassNo(),
+                user.getGrade(),
+                user.getUserRole()
+        );
     }
 }
