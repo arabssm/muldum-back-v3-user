1daf89ddc0bfcccd1a6a3e1973ed4872749305ec
randirao <randirao4245@gmail.com>
2025-11-03 19:25:44 +0900

    feat: GoogleLoginResponse에 refreshToken 필드 추가

--- src/main/java/co/kr/muldum/adapter/in/web/dto/GoogleLoginResponse.java
+++ src/main/java/co/kr/muldum/adapter/in/web/dto/GoogleLoginResponse.java
@@ -14,13 +14,15 @@ public class GoogleLoginResponse {
     private final String name;
     private final Long teamId;
     private final String accessToken;
+    private final String refreshToken;
 
-    private GoogleLoginResponse(Role role, Long userId, String name, Long teamId, String accessToken) {
+    private GoogleLoginResponse(Role role, Long userId, String name, Long teamId, String accessToken, String refreshToken) {
         this.role = role;
         this.userId = userId;
         this.name = name;
         this.teamId = teamId;
         this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
     }
 
     public static GoogleLoginResponse from(LoginResponse loginResponse) {
@@ -29,7 +31,8 @@ public class GoogleLoginResponse {
             loginResponse.getUserId(),
             loginResponse.getName(),
             loginResponse.getTeamId(),
-            loginResponse.getAccessToken()
+            loginResponse.getAccessToken(),
+            loginResponse.getRefreshToken()
         );
     }
 }
