36768d160e36b72bbb70e8f3803dc6e5e988b620
randirao <randirao4245@gmail.com>
2025-11-03 17:44:03 +0900

    refactor: UserJpaRepository 메서드명 변경 및 findAllByEmailIgnoreCase 추가

--- src/main/java/co/kr/muldum/adapter/out/persistence/UserJpaRepository.java
+++ src/main/java/co/kr/muldum/adapter/out/persistence/UserJpaRepository.java
@@ -1,9 +1,9 @@
 package co.kr.muldum.adapter.out.persistence;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserJpaRepository extends JpaRepository<UserJpaEntity, Long> {
-    Optional<UserJpaEntity> findByEmail(String email);
+    Optional<UserJpaEntity> findByEmailIgnoreCase(String email);
+    Optional<UserJpaEntity> findAllByEmailIgnoreCase(String email);
 }
