2ead4cf7dd168e75277012055a9b15a4d8ec3526
randirao <randirao4245@gmail.com>
2025-11-03 19:24:58 +0900

    feat: RefreshToken 생성 Provider 구현

---
+++ src/main/java/co/kr/muldum/adapter/out/token/RefreshTokenProvider.java
@@ -0,0 +1,27 @@
+package co.kr.muldum.adapter.out.token;
+
+import co.kr.muldum.application.port.out.RefreshTokenPort;
+import co.kr.muldum.domain.model.RefreshToken;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Component
+public class RefreshTokenProvider implements RefreshTokenPort {
+
+    private final long refreshTokenExpiration;
+
+    public RefreshTokenProvider(@Value("${jwt.refresh-token-expiration}") long refreshTokenExpiration) {
+        this.refreshTokenExpiration = refreshTokenExpiration;
+    }
+
+    @Override
+    public RefreshToken generateRefreshToken(Long userId) {
+        String token = UUID.randomUUID().toString();
+        LocalDateTime expiryDate = LocalDateTime.now().plusSeconds(refreshTokenExpiration / 1000);
+
+        return RefreshToken.of(token, userId, expiryDate);
+    }
+}
