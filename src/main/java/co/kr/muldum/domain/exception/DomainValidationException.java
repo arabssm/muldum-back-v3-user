f87bb63ba8c454759a2cd9ec8766f6850a8831a9
randirao <randirao4245@gmail.com>
2025-11-05 11:42:17 +0900

    feat: 도메인 검증 예외 기본 클래스 추가

---
+++ src/main/java/co/kr/muldum/domain/exception/DomainValidationException.java
@@ -0,0 +1,12 @@
+package co.kr.muldum.domain.exception;
+
+public class DomainValidationException extends RuntimeException {
+
+    public DomainValidationException(String message) {
+        super(message);
+    }
+
+    public DomainValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
