881053cce330df64e194c4a4fa1b678608231403
randirao <randirao4245@gmail.com>
2025-11-05 15:09:38 +0900

    refactor: User 생성자를 protected로 전환

--- src/main/java/co/kr/muldum/domain/model/User.java
+++ src/main/java/co/kr/muldum/domain/model/User.java
@@ -1,6 +1,5 @@
 package co.kr.muldum.domain.model;
 
-import co.kr.muldum.domain.validator.UserValidator;
 import lombok.Getter;
 
 import java.util.UUID;
@@ -16,8 +15,14 @@ public class User {
     private final Role userRole;
     private final Long teamId; // Member 테이블에서 조회한 값
 
-    private User(UUID id, String name, Integer enrolledAt, String email,
-                Integer classNo, Integer grade, Role userRole, Long teamId) {
+    protected User(UUID id,
+                   String name,
+                   Integer enrolledAt,
+                   String email,
+                   Integer classNo,
+                   Integer grade,
+                   Role userRole,
+                   Long teamId) {
         this.id = id;
         this.name = name;
         this.enrolledAt = enrolledAt;
@@ -26,13 +31,5 @@ public class User {
         this.grade = grade;
         this.userRole = userRole;
         this.teamId = teamId;
-    }
-
-    public static User of(UUID id, String name, Integer enrolledAt, String email,
-                          Integer classNo, Integer grade, Role userRole, Long teamId) {
-        UserValidator.validateEmail(email);
-        UserValidator.validateName(name);
-
-        return new User(id, name, enrolledAt, email, classNo, grade, userRole, teamId);
     }
 }
