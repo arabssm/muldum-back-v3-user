eabbaad5ae1ba28a6bdf89311e2b144c55c2bda9
randirao <randirao4245@gmail.com>
2025-11-05 15:11:31 +0900

    refactor: RefreshTokenValidationException에 에러 코드와 details 추가

--- src/main/java/co/kr/muldum/domain/exception/RefreshTokenValidationException.java
+++ src/main/java/co/kr/muldum/domain/exception/RefreshTokenValidationException.java
@@ -1,12 +1,24 @@
 package co.kr.muldum.domain.exception;
 
+import java.util.Map;
+
 public class RefreshTokenValidationException extends DomainValidationException {
 
+    private static final String ERROR_CODE = "REFRESH_TOKEN_VALIDATION_ERROR";
+
     public RefreshTokenValidationException(String message) {
-        super(message);
+        super(ERROR_CODE, message);
+    }
+
+    public RefreshTokenValidationException(String message, Map<String, Object> details) {
+        super(ERROR_CODE, message, details);
     }
 
     public RefreshTokenValidationException(String message, Throwable cause) {
-        super(message, cause);
+        super(ERROR_CODE, message, cause);
+    }
+
+    public RefreshTokenValidationException(String message, Map<String, Object> details, Throwable cause) {
+        super(ERROR_CODE, message, details, cause);
     }
 }
