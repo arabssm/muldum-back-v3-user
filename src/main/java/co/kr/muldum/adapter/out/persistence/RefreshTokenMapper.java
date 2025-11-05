cff4e0ff6980ef341f22072d6b4302badf3fcf8e
randirao <randirao4245@gmail.com>
2025-11-05 15:13:27 +0900

    refactor: RefreshTokenMapper에 Factory 주입

--- src/main/java/co/kr/muldum/adapter/out/persistence/RefreshTokenMapper.java
+++ src/main/java/co/kr/muldum/adapter/out/persistence/RefreshTokenMapper.java
@@ -1,18 +1,27 @@
 package co.kr.muldum.adapter.out.persistence;
 
 import co.kr.muldum.domain.model.RefreshToken;
+import co.kr.muldum.domain.model.RefreshTokenFactory;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RefreshTokenMapper {
 
-    public static RefreshToken toDomain(RefreshTokenJpaEntity entity) {
-        return RefreshToken.of(
+    private final RefreshTokenFactory refreshTokenFactory;
+
+    public RefreshTokenMapper(RefreshTokenFactory refreshTokenFactory) {
+        this.refreshTokenFactory = refreshTokenFactory;
+    }
+
+    public RefreshToken toDomain(RefreshTokenJpaEntity entity) {
+        return refreshTokenFactory.create(new RefreshTokenFactory.RefreshTokenCreateCommand(
                 entity.getToken(),
                 entity.getUserId(),
                 entity.getExpiryDate()
-        );
+        ));
     }
 
-    public static RefreshTokenJpaEntity toEntity(RefreshToken refreshToken) {
+    public RefreshTokenJpaEntity toEntity(RefreshToken refreshToken) {
         return RefreshTokenJpaEntity.of(
                 refreshToken.getToken(),
                 refreshToken.getUserId(),
