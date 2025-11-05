e4e731a8b760576d0140df9c161e4cc6b3024dd4
randirao <randirao4245@gmail.com>
2025-11-05 14:40:36 +0900

    config: ConfigurationProperties 스캔 활성화

--- src/main/java/co/kr/muldum/MuldumApplication.java
+++ src/main/java/co/kr/muldum/MuldumApplication.java
@@ -2,8 +2,10 @@ package co.kr.muldum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class MuldumApplication {
 
     public static void main(String[] args) {
