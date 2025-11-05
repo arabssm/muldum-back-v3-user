2135b2060a34dde16c19b39dc9d9bab2ea0bb305
randirao <randirao4245@gmail.com>
2025-11-05 11:13:26 +0900

    refactor: WebConfig를 infrastructure/config로 이동

--- src/main/java/co/kr/muldum/infrastructure/config/WebConfig.java
+++ src/main/java/co/kr/muldum/infrastructure/config/WebConfig.java
@@ -1,4 +1,4 @@
-package co.kr.muldum.adapter.in.web;
+package co.kr.muldum.infrastructure.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
