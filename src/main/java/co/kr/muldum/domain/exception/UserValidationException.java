323b6519085f29f28b60f1ad126942779b9c3a0b
randirao <randirao4245@gmail.com>
2025-11-05 15:10:57 +0900

    refactor: UserValidationException에 에러 코드와 details 추가

--- src/main/java/co/kr/muldum/domain/exception/UserValidationException.java
+++ src/main/java/co/kr/muldum/domain/exception/UserValidationException.java
@@ -1,12 +1,24 @@
 package co.kr.muldum.domain.exception;
 
+import java.util.Map;
+
 public class UserValidationException extends DomainValidationException {
 
+    private static final String ERROR_CODE = "USER_VALIDATION_ERROR";
+
     public UserValidationException(String message) {
-        super(message);
+        super(ERROR_CODE, message);
+    }
+
+    public UserValidationException(String message, Map<String, Object> details) {
+        super(ERROR_CODE, message, details);
     }
 
     public UserValidationException(String message, Throwable cause) {
-        super(message, cause);
+        super(ERROR_CODE, message, cause);
+    }
+
+    public UserValidationException(String message, Map<String, Object> details, Throwable cause) {
+        super(ERROR_CODE, message, details, cause);
     }
 }
