0692ff2bc87acb566f0f25b1a2b10d08f37b8882
randirao <randirao4245@gmail.com>
2025-11-05 15:09:49 +0900

    refactor: RefreshToken 생성자를 protected로 전환

--- src/main/java/co/kr/muldum/domain/model/RefreshToken.java
+++ src/main/java/co/kr/muldum/domain/model/RefreshToken.java
@@ -1,6 +1,5 @@
 package co.kr.muldum.domain.model;
 
-import co.kr.muldum.domain.validator.RefreshTokenValidator;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -12,18 +11,10 @@ public class RefreshToken {
     private final UUID userId;
     private final LocalDateTime expiryDate;
 
-    private RefreshToken(String token, UUID userId, LocalDateTime expiryDate) {
+    protected RefreshToken(String token, UUID userId, LocalDateTime expiryDate) {
         this.token = token;
         this.userId = userId;
         this.expiryDate = expiryDate;
-    }
-
-    public static RefreshToken of(String token, UUID userId, LocalDateTime expiryDate) {
-        RefreshTokenValidator.validateToken(token);
-        RefreshTokenValidator.validateUserId(userId);
-        RefreshTokenValidator.validateExpiryDate(expiryDate);
-
-        return new RefreshToken(token, userId, expiryDate);
     }
 
     public boolean isExpired() {
